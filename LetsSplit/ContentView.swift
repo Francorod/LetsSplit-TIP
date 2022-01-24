8ed5f17d718c3cc19ccb486856d2a5cc6e619fe0
Franco Rod <81878824+Francorod@users.noreply.github.com>
2022-01-24 09:14:22 -0500

    Initial Commit

---
+++ LetsSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LetsSplit
+//
+//  Created by Franco Rodrigues on 1/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LetsSplit/LetsSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsSplitApp.swift
+//  LetsSplit
+//
+//  Created by Franco Rodrigues on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct LetsSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
